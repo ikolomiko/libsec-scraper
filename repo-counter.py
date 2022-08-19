f10607b54ac68b58de12873c05baf4a6584e88ff
İlker Avcı <ikolomiko@gmail.com>
2022-08-19 22:53:45 +0300

    Update stats

--- repo-counter.py
+++ repo-counter.py
@@ -12,13 +12,15 @@ def extract_baseurl(s: str, group_id: str) -> str:
 
 TinyDB.table_class = StringIdClassTable
 
-dc = defaultdict(int)
+dc = defaultdict(lambda: ["", 0]) # URL: (NAME, USAGE)
 for db in ["db.json", "db2.json", "db3.json", "db4.json", "db5.json", "db6.json", "db7.json", "db8.json", "db9.json"]:
     database = TinyDB(db)
     for item in database.all():
-        repo = (item['repo'], extract_baseurl(item['base_url'], item['group_id']))
-        dc[repo] += 1
+        url = extract_baseurl(item['base_url'], item['group_id'])
+        name = item['repo']
+        dc[url][0] = name
+        dc[url][1] += 1
 
 with open("repo_stats.csv", 'w', encoding = 'utf-8') as f:
-    for k, v in sorted(dc.items(), key=lambda item: item[1], reverse=True):
-        f.write(f"{k[0]},{v},{k[1]}\n")
+    for k, v in sorted(dc.items(), key=lambda item: item[1][1], reverse=True):
+        f.write(f"{v[0]},{v[1]},{k}\n")
