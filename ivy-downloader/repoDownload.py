bb22cfd6dd8c93072f5414c421cfd1655fd93456
İlker Avcı <ikolomiko@gmail.com>
2022-11-15 22:26:32 +0300

    Last attempts at ivy (unsuccessful)

--- ivy-downloader/repoDownload.py
+++ ivy-downloader/repoDownload.py
@@ -6,47 +6,46 @@ with open("repo_stats.csv", 'r', encoding='utf-8') as f:
 
 with open("ivy-public.xml", 'w', encoding='utf-8') as f:
     header = """<ivysettings>
-             <resolvers>"""
+    <resolvers>
+"""
     f.write(header)
 
-    chainCommon = """<chain name="public-chain" returnFirst="true">
+    addition = """
+        <ibiblio name="google-android-aar" m2compatible="true" root="https://dl.google.com/dl/android/maven2/" pattern="[organization]/[module]/[revision]/[module]-[revision].aar"/>
+        <ibiblio name="google-android-jar" m2compatible="true" root="https://dl.google.com/dl/android/maven2/" pattern="[organization]/[module]/[revision]/[module]-[revision].[ext]" />
+        <ibiblio name="mavenorg-aar" m2compatible="true" root="https://repo1.maven.org/maven2/" pattern="[organization]/[module]/[revision]/[module]-[revision].aar" />
+        <ibiblio name="mavenorg-jar" m2compatible="true" />
+"""
+    f.write(addition)
+
+    for line in lines:
+        (name, usages, url) = line.strip().split(',')
+        data = f"""
+        <ibiblio name="{name}-aar" m2compatible="true" root="{url}" pattern="[organization]/[module]/[revision]/[module]-[revision].aar" />
+        <ibiblio name="{name}-jar" m2compatible="true" root="{url}" pattern="[organization]/[module]/[revision]/[module]-[revision].[ext]" />
+"""
+        f.write(data)
+
+    chainCommon = """
+        <chain name="public-chain" returnFirst="true">
                 <resolver ref="mavenorg-aar"/>
-                <resolver ref="public"/>
+                <resolver ref="mavenorg-jar"/>
                 <resolver ref="google-android-aar"/>
-                <resolver ref="google-android-jar"/>"""
+                <resolver ref="google-android-jar"/>
+"""
     f.write(chainCommon)
     for line in lines:
         name = line.strip().split(',')[0]
-        chainInternal = f""" <resolver ref="{name}-aar"/>
-                        <resolver ref="{name}-jar"/>
-                        """
+        chainInternal = f"""
+                <resolver ref="{name}-aar"/>
+                <resolver ref="{name}-jar"/>
+"""
         f.write(chainInternal)
 
     chainClose = """</chain>"""
     f.write(chainClose)
 
-    addition = """<url name="google-android-aar" m2compatible="true">
-            <artifact pattern="https://dl.google.com/dl/android/maven2/[organization]/[module]/[revision]/[module]-[revision].aar"/>
-        </url>
-        <url name="google-android-jar" m2compatible="true">
-            <artifact pattern="https://dl.google.com/dl/android/maven2/[organization]/[module]/[revision]/[module]-[revision].jar"/>
-        </url>
-        <url name="mavenorg-aar" m2compatible="true">
-            <artifact pattern="https://repo1.maven.org/maven2/[organization]/[module]/[revision]/[module]-[revision].aar"/>
-        </url>
-        <ibiblio name="public" m2compatible="true"/>"""
-    f.write(addition)
-
-    for line in lines:
-        (name, usages, url) = line.strip().split(',')
-        data = f"""<url name="{name}-aar" m2compatible="true">
-                           <artifact pattern="{url}/[organization]/[module]/[revision]/[module]-[revision].aar"/>
-                       </url>
-                       <url name="{name}-jar" m2compatible="true">
-                           <artifact pattern="{url}/[organization]/[module]/[revision]/[module]-[revision].jar"/>
-                       </url>"""
-        f.write(data)
-
-    footer = """</resolvers>
-    </ivysettings>"""
+    footer = """
+    </resolvers>
+</ivysettings>"""
     f.write(footer)
